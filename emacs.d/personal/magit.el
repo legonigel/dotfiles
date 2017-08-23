e4fd3067677c1cee3df6c84a0d5333b2e467d1c8
Nigel Armstrong <legonigel@gmail.com>
2017-08-22 20:36:12 -0400

    Changing some settings that broke magit in some version

--- emacs.d/personal/magit.el
+++ emacs.d/personal/magit.el
@@ -40,9 +40,9 @@
 
 (eval-after-load 'magit
   '(progn
-     (set-face-background 'magit-item-highlight "#3f4747")
-     (set-face-foreground 'magit-item-highlight nil)
-     (set-face-underline  'magit-item-highlight nil)
+     ;(set-face-background 'magit-item-highlight "#3f4747")
+     ;(set-face-foreground 'magit-item-highlight nil)
+     ;(set-face-underline  'magit-item-highlight nil)
      (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
      (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
      (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
